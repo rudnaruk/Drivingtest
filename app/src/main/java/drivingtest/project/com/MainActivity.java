7bf7257f920d6a0647fd6a167c649b51fe90029f
piyaponf <piyaponf@admins-MacBook-Pro-5.local>
2017-11-08 11:13:01 +0700

    update

--- app/src/main/java/drivingtest/project/com/MainActivity.java
+++ app/src/main/java/drivingtest/project/com/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         if(i == R.id.button) {
             Intent intent = new Intent(this, TestActivity.class);
             intent.putExtra("cat_id",1);
-            intent.putExtra("test_type",TestActivity.TYPE_PRE_TEST);
+            intent.putExtra("test_type",TestActivity.TYPE_POST_TEST);
             intent.putExtra("time_mode", TestActivity.MODE_TIME_COUNTER);
             startActivity(intent);
         }else if(i == R.id.button1){
             Intent intent = new Intent(this, TestActivity.class);
             intent.putExtra("cat_id",1);
-            intent.putExtra("test_type",TestActivity.TYPE_POST_TEST);
+            intent.putExtra("test_type",TestActivity.TYPE_PRE_TEST);
             intent.putExtra("time_mode", TestActivity.MODE_NO_TIMEE);
             startActivity(intent);
         }else{
